1815a205c0cbe7e912ec66631cec7fd621907f4c
JoelSANQ <joelsanchequi@gmail.com>
2025-03-07 06:20:25 -0600

    PriceTag Added

--- lib/icons/tabs/donut_tab.dart
+++ lib/icons/tabs/donut_tab.dart
@@ -2,39 +2,37 @@ import 'package:donut_app_2b_sanchez/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class DonutTab extends StatelessWidget {
-
-  //lista de donas
-  final List donutsOnsale = [
-    // [donutFlavor, donutPrice, donutColor, imageName]
+  //Lista de donas
+  final List donutsOnSale = [
+    //[donutFlavor, donutPrice, donutColor, imageName]
     ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-     ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-      ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-       ["Choco", "36", Colors.brown, "lib/images/chocolate_donut.png"],
-  
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
   ];
-   DonutTab({super.key});
+
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Para acomodar elementos en cuadricula
     return GridView.builder(
-      //Elementos en nuestra lista ///cuantos elementos tengo
-      itemCount: donutsOnsale.length,
-      padding: const EdgeInsets.all(8.0),
-      //organiza como distribuir
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //numero de columnas
-        crossAxisCount: 2),
+        //Cuantos elementos tengo
+        itemCount: donutsOnSale.length,
+        padding: const EdgeInsets.all(8.0),
+        //Prepa 1: Organiza como distribuir elementos en la cuadricula
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //Numero de colummnas
+            crossAxisCount: 2,
+            //Relación de aspecto (Proporción)
+            childAspectRatio: 1 / 1.5),
         itemBuilder: (context, index) {
-         return DonutTile(
-          donutFlavor: donutsOnsale[index][0],
-          donutPrice: donutsOnsale[index][1],
-          donutColor: donutsOnsale[index][2],
-          imageName: donutsOnsale[index][3],
-         );
-       
-          
-        }
-    )
-    ;
+          return DonutTile(
+            donutFLavor: donutsOnSale[index][0],
+            donutPrice: donutsOnSale[index][1],
+            donutColor: donutsOnSale[index][2],
+            imageName: donutsOnSale[index][3],
+          );
+        });
   }
-}///
+}
--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
 
 class DonutTile extends StatelessWidget {
-  final String donutFlavor;
+  final String donutFLavor;
   final String donutPrice;
+  //dynamic sera un tipo de color
   final dynamic donutColor;
   final String imageName;
-  const DonutTile({super.key, required this.donutFlavor, required this.donutPrice, this.donutColor, required this.imageName});
+
+  const DonutTile(
+      {super.key,
+      required this.donutFLavor,
+      required this.donutPrice,
+      this.donutColor,
+      required this.imageName});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: donutColor[500]
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        decoration: BoxDecoration(color: donutColor[100],
+        borderRadius: BorderRadius.circular(24)
+        ),
+        child: Column(children: [
+          //PriceTag
+          Row(
+            //Alinea a la derecha
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: donutColor[200],
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(24),
+                    bottomLeft: Radius.circular(24)),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 18
+                ),
+                child: Text('\$$donutPrice',
+                style: TextStyle(fontWeight:
+                FontWeight.bold,
+                fontSize: 18,
+                color: donutColor[800]
+                ),
+                ),
+              )
+            ],
+          ),
+          //Donut Picture
+          Padding(padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+          child: Image.asset(imageName),
+          )
+
+          //Donut Text
+
+          //Love icon + add button
+        ]
+      ),
       ),
     );
   }
